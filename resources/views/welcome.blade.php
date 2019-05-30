ead15141838f62c7bb8158b3efe68cda6f435e8f
Hugo Hernández <hugohernandez@MacBook-Pro-de-Hugo.local>
2019-05-30 02:13:04 -0500

    adding new structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,32 +48,13 @@
           </pre>
         </div>
       </div>
-      <div class="feature">
-        <div class="feature__item">
-          <h3 class="section__title">Fast & Light</h3>
-          <p>Start writing your notes immediately in any terminal! No more time wasted on navigating and opening your text editor.</p>
-        </div>
-        <div class="feature__item">
-          <h3 class="section__title">File Syncing</h3>
-          <p>Save your file in Dropbox then you can access to it from anywhere.</p>
-        </div>
-        <div class="feature__item">
-          <h3 class="section__title">Secure</h3>
-          <p>Encrypt your notes optionally. No one can get to your secrets! </p>
-        </div>
-        <div class="feature__item">
-          <h3 class="section__title">Configuration</h3>
-          <p>Maintain all your settings in a single <span class="code code--inline">config.json</span> file. Never need to redo the setting every single time jotting down a note.</p>
-        </div>
-        <div class="feature__item">
-          <h3 class="section__title">Highlightings</h3>
-          <p>For better readability, scribbler has a clean, beautiful color scheme allow you to scan files fast.</p>
-        </div>
-        <div class="feature__item">
-          <h3 class="section__title">Keybindings</h3>
-          <p>You can expect common keybindings for scribbler. Customize <span class="code code--inline">bindings.json</span> for your own liking! </p>
-        </div>
-      </div>
+
+
+      @include('sections.features')
+
+      
+
+
       <div class="keybinding">
         <ul class="keybinding__detail">
           <h3 class="keybinding__title">Default Keybindings</h3>
