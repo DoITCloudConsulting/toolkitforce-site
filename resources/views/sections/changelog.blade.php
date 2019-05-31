7b12c14a9d0ccbad8d4b72ea84f8b54062b63609
Hugo Hernández <hugo@doitcloud.consulting>
2019-05-30 19:38:05 -0500

    adding changes to changelog level section

---
+++ resources/views/sections/changelog.blade.php
@@ -0,0 +1,22 @@
+<div class="changelog">
+      <div class="wrapper">
+        <h3 class="section__title">Changelog</h3>
+        <div class="changelog__item">
+          <div class="changelog__meta">
+            <h4 class="changelog__title">0.0.2</h4>
+            <small class="changelog__date"> 2019-04-30 06:08 UTC</small>
+          </div>
+          <div class="changelog__detail">
+            <ul>
+              <li>First stable version</li>
+              <li>Integration SOAP Service</li>
+              <li>Config file to credentials and validations to entry fields.</li>
+            </ul>
+          </div>
+        </div>
+        
+        <div class="changelog__callout">
+          <a href="#" class="button--secondary disabled">Checkout Full Log</a>
+        </div>
+      </div>
+    </div>
